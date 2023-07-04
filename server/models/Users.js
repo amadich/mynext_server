ee4df0dae78c99647dc1ba1dcedf22b445c4bca7
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-07-04 21:52:47 +0100

    Update Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -2,7 +2,8 @@ const {Schema , model} = require("mongoose");
 const UserSchema = Schema({
    email: {type : String , require : true},
    password : {type : String , require : true},
-   avatar : {type : String , require : true}
+   avatar : {type : String , require : true},
+   dateCreate: {type: String , require: true}
 })
 const UserModel = model("accunets",UserSchema);
 module.exports = UserModel;
